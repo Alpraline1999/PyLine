e90a8a790c1ef42a1385d15d79c79d5456724024
Alpraline1999 <1776882398@qq.com>
2024-08-15 15:28:54 +0800

    pyline v0.1.0

--- src/pyline.py
+++ src/pyline.py
@@ -1,21 +1,31 @@
 # pyline.py
 import tkinter as tk
 from tkinter import filedialog, colorchooser, simpledialog, Menu, ttk
-from PIL import Image
+from PIL import Image, ImageTk
 import sys
+import base64
+from io import BytesIO
 import mouseset
 import drawphoto
+import icon
 
 
 class PyLine:
     def __init__(self, root):
         self.root = root
         self.root.title("PyLine")
-        self.root.geometry("1170x730")
+        self.root.geometry("1170x750")
         self.root.resizable(False, False)
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         self.mouse_set = mouseset.MouseSensitivitySet()
+
+        # set icon
+        icon_base64 = icon.icon_data().icon_base64
+        icon_data = base64.b64decode(icon_base64)
+        icon_image = Image.open(BytesIO(icon_data))
+        icon_image = ImageTk.PhotoImage(icon_image)
+        self.root.iconphoto(False, icon_image)
 
         self._init_variables()
         self._create_frame()
